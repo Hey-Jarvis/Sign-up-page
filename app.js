54526461422393405c19fb4c3aab157e8c76144d
Its-Holmes <shubham.raddy20@outlook.com>
2021-11-19 12:51:12 +0530

    just hide some files

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const client = require("@mailchimp/mailchimp_marketing");
 const { response } = require("express");
 const express = require ("express");
 // const request = require("request");    for olde version of node
-const https = require("https");
+
 // const { post } = require("request");    its the auto part of babel
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -14,58 +17,60 @@ app.use(express.urlencoded({extended: true}));
 
 client.setConfig({
 
-    apiKey: "e44ebf8292f86235c74f0a0a92ab22e7-us6",
-  
-    server: "us6",
-  
-  });
+  apiKey: process.env.API_KEY,
+
+  server: process.env.SERVER
+
+});
 
 app.get("/", (req,res) =>{
-    res.sendFile(__dirname + "/signup.html")
+  res.sendFile(__dirname + "/signup.html")
 })
 
 
 app.post("/",(req,res)=>{
 
-    const firstName = req.body.fname;
+  const firstName = req.body.fname;
   
-    const lastName = req.body.lname;
+  const lastName = req.body.lname;
   
-    const email = req.body.email;
+  const email = req.body.email;
   
-    const subscribingUser = {fname: firstName, lname: lastName, email: email}
+  const subscribingUser = {fname: firstName, lname: lastName, email: email}
   
-    const run = async () => {
-  
-     const response = await client.lists.addListMember("026f92d3d0", {
-  
-       email_address: subscribingUser.email,
-  
-       status: "subscribed",
-  
-       merge_fields: {
-  
-           FNAME: subscribingUser.fname,
-  
-           LNAME: subscribingUser.lname
-       }
-     });
+  const run = async () => {
 
-     res.sendFile(__dirname + "/success.html")
-     console.log(response)
+    const lid = process.env.LIST_ID;
+  
+    const response = await client.lists.addListMember(lid, {
+
+      email_address: subscribingUser.email,
+  
+      status: "subscribed",
+  
+      merge_fields: {
+
+        FNAME: subscribingUser.fname,
+  
+        LNAME: subscribingUser.lname
+      }
+    });
+
+    res.sendFile(__dirname + "/success.html")
+    console.log(response)
   
   };
 
-  
+
   run().catch(e => res.sendFile(__dirname + "/failure.html"));
   
-  });
+});
 
-  app.post("/failure", (req,res)=>{
-    res.redirect("/");
-  });
+app.post("/failure", (req,res)=>{
+  res.redirect("/");  
+});
   
 app.listen(3000, (req,res)=>{
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
 
